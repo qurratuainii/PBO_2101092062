c0fe3250398ba0794ef7e9447d7023b1af0ff1a0
qurratuainii <114158317+qurratuainii@users.noreply.github.com>
2022-09-30 11:05:28 +0700

    Update Latihan1.java

--- src/Aini300922/Latihan1.java
+++ src/Aini300922/Latihan1.java
@@ -13,6 +13,7 @@ package Aini300922;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+///jika ingin 3 buah diatas ganti aja dengan import java.io.*;
 
 public class Latihan1 {
     public static void main(String[] args) {
@@ -20,14 +21,24 @@ public class Latihan1 {
                 new InputStreamReader(System.in));
         
         String name = "";
+        String address = "";
+        String jurusan = "";
         
-        System.out.print("Please Enter Your Name: ");
         
         try{
+            System.out.print("Please Enter Your Name: ");
             name = dataIn.readLine();
+  
+            System.out.print("Please Enter Your Address: ");
+            address = dataIn.readLine();
+         
+            System.out.print("Please Enter Your Major: ");
+            jurusan = dataIn.readLine();
             }catch (IOException e){
             System.out.print("Error!");
         }
-        System.out.println("Hello " + name +"!");
+        System.out.println("Hello " + name +"!" +
+                           "\nAlamat saya  " + address +
+                           "\nJurusan saya " + jurusan);
     }
 }
