a13bce6d92532f6bdb69323069ae426e7a7c6602
qurratuainii <114158317+qurratuainii@users.noreply.github.com>
2022-10-03 19:41:33 +0700

    Update Latihan3.java

--- src/Aini300922/Latihan3.java
+++ src/Aini300922/Latihan3.java
@@ -9,6 +9,18 @@ package Aini300922;
  *
  * @author HP Folio 1040
  */
+
+import javax.swing.JOptionPane;
+
 public class Latihan3 {
-    
+    public static void main(String[] args) {
+        
+        String name = "";
+        
+        name = JOptionPane.showInputDialog("Please enter your name");
+        
+        String msg = "Hello " + name + "!";
+        
+        JOptionPane.showMessageDialog(null, msg);
+    }
 }
