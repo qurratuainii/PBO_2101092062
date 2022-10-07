29e58f896598357f1e808fac138f2fbf79d827a3
qurratuainii <114158317+qurratuainii@users.noreply.github.com>
2022-10-07 21:32:57 +0700

    Create Latihan2.java

---
+++ src/Aini071022/Latihan2.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aini071022;
+
+/**
+ *
+ * @author HP Folio 1040
+ */
+public class Latihan2 {
+    public static void main(String[] args) {
+        int grade = 92;
+        
+        switch(grade){
+            case 100:
+                System.out.println("Excellent!");
+                break;
+            
+            case 90:
+                System.out.println("Good job!");
+                break;
+              
+            case 80: 
+                System.out.println("Study harder!");
+                break;
+            
+            default:
+                System.out.println("Sorry, you failde.");
+            
+        }
+    }
+}
