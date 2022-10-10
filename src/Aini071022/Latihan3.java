4cc58c8056cd8766e81605f88d83764b7f8a709f
qurratuainii <114158317+qurratuainii@users.noreply.github.com>
2022-10-10 10:26:46 +0700

    Update Latihan3.java

--- src/Aini071022/Latihan3.java
+++ src/Aini071022/Latihan3.java
@@ -10,32 +10,6 @@ package Aini071022;
  * @author HP Folio 1040
  */
 
-import java.io.*;
-public class Latihan3 {
-    public static void main(String[] args) {
-        BufferedReader dataIn = new BufferedReader(
-                new InputStreamReader(System.in));
-    
-        String gol = " ";
-        int gpokok = 0;
-        int gjam = 0;
-        int jam = 0;
-        
-        try{
-            System.out.print("Golongan : ");
-            gol = dataIn.readLine();
-            
-            System.out.print("Gaji Pokok : ");
-            gpokok = Int1eger.parseInt(dataIn.readLine());
-            
-            System.out.print("Gaji Perjam : ");
-            gjam = Integer.parseInt(dataIn.readLine());
-        }catch (IOException e){
-            System.out.println("Error!");
-        }
-        
-        switch(gol){{}
-  
-}
-    
 
+public class Latihan3 {
+}
