9d497c587c49d0b6b4b40d53021ea53bc05f91ab
qurratuainii <114158317+qurratuainii@users.noreply.github.com>
2022-10-27 20:58:27 +0700

    Create Latihan4.java

---
+++ src/Aini211022/Latihan4.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aini211022;
+
+/**
+ *
+ * @author HP Folio 1040
+ */
+
+import javax.swing.JOptionPane;
+
+public class Latihan4 {
+    public static void main(String[] args) {
+        String a="";
+        String b="";
+        
+        a = JOptionPane.showInputDialog("Masukkan angka");
+     
+        b = JOptionPane.showInputDialog("Masukkan pangkat");
+        
+        
+        
+    }
+}
