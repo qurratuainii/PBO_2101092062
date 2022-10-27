fef48240832f9d297b02684263feb6fa7ed9538c
qurratuainii <114158317+qurratuainii@users.noreply.github.com>
2022-10-27 20:58:24 +0700

    Create Latihan2.java

---
+++ src/Aini211022/Latihan2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aini211022;
+
+/**
+ *
+ * @author HP Folio 1040
+ */
+public class Latihan2 {
+    public static void main(String[] args) {
+        
+        int i=1;
+        
+        do {
+            System.out.println(i+".\tQurratu Aini");
+            i++;
+        }while (i<=100);
+    }
+}
