3a2844560ff5a5949425502adfcd11347d689173
qurratuainii <114158317+qurratuainii@users.noreply.github.com>
2022-10-28 20:39:46 +0700

    Create Latihan5.java

---
+++ src/Aini281022/Latihan5.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aini281022;
+
+/**
+ *
+ * @author HP Folio 1040
+ */
+public class Latihan5 {
+    public static void main(String[] args) {
+        System.out.println("Number of arguments = "+args.length);
+        System.out.println("First Argument = "+args[0]);
+    }
+}
