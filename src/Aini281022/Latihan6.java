7f5ef87ce6f2321ad87fae7bbc67cf969acb73db
qurratuainii <114158317+qurratuainii@users.noreply.github.com>
2022-10-28 20:39:49 +0700

    Create Latihan6.java

---
+++ src/Aini281022/Latihan6.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aini281022;
+
+/**
+ *
+ * @author HP Folio 1040
+ */
+public class Latihan6 {
+    public static void main(String[] args) {
+        int i;
+        
+        for(i=1; i<args.length; i++){
+            System.out.println(args[i]);
+        }
+    }
+}
