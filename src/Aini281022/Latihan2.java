7c7e4f0a2ef0264046d3b3305938fd566c9029b3
qurratuainii <114158317+qurratuainii@users.noreply.github.com>
2022-10-28 20:39:37 +0700

    Create Latihan2.java

---
+++ src/Aini281022/Latihan2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aini281022;
+
+/**
+ *
+ * @author HP Folio 1040
+ */
+public class Latihan2 {
+    public static void main(String[] args) {
+        int[] ages = new int [100];
+        
+        String days[] = {"Monday", "Tuesday", "Wednesday", "Thursday",
+                         "Friday", "Saturday", "Sunday"};
+      
+        for (int i=0; i<days.length; i++){
+            System.out.println(days[i]);
+            
+        }
+    }
+}
