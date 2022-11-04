6d0210e4f3661d61ee86572c3cc714e38ec77d1b
qurratuainii <114158317+qurratuainii@users.noreply.github.com>
2022-11-04 11:46:50 +0700

    Update StudentRecordExample.java

--- src/Aini041122/StudentRecordExample.java
+++ src/Aini041122/StudentRecordExample.java
@@ -10,5 +10,25 @@ package Aini041122;
  * @author HP Folio 1040
  */
 public class StudentRecordExample {
-    
+    public static void main(String[] args) {
+        StudentRecord student1 = new StudentRecord();
+        //
+        student1.setName("Ani");
+        student1.setAddress("Padang");
+        student1.setAge(20);
+        student1.setMathGrade(70);
+        student1.setEnglishGrade(80);
+        student1.setScienceGrade(70);
+        //
+        
+        System.out.println("Nama           : "+student1.getName());
+        System.out.println("Alamat         : "+student1.getAddress());
+        System.out.println("Umur           : "+student1.getAge());
+        System.out.println("Matematika     : "+student1.getMathGrade());
+        System.out.println("B.Inggris      : "+student1.getEnglishGrade());
+        System.out.println("Pengetahuan    : "+student1.getScienceGrade());
+        System.out.println("Rata-rata      : "+student1.getAverage());
+        System.out.println("Maximum        : "+student1.getMax());
+        
+    }
 }
