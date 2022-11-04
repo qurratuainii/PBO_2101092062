25dd2523cfbd89530735219a385c90aadd1d499a
qurratuainii <114158317+qurratuainii@users.noreply.github.com>
2022-11-04 11:09:30 +0700

    Create StudentRecord.java

---
+++ src/Aini041122/StudentRecord.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aini041122;
+
+/**
+ *
+ * @author HP Folio 1040
+ */
+public class StudentRecord {
+    private String name;
+    private String address;
+    private int age;
+    private double mathGrade;
+    private double englishGrade;
+    private double scienceGrade;
+    private double average;
+    
+    private static int studentCount;
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getAddress(){
+        return address;
+    }
+    
+    public void setAddress(String address){
+        this.address = address;
+    }
+    
+    public int getAge(){
+        return age;
+    }
+    
+    public void setAge(int age){
+        this.age = age;
+    }
+    
+    public double getMathGrade(){
+        return mathGrade;
+    }
+    
+    public void setMathGrade(double mathGrade){
+        this.mathGrade = mathGrade;
+    }
+    
+    public double getEnglishGrade(){
+        return englishGrade;
+    }
+    
+    public void setEnglishGrade(double englishGrade){
+        this.englishGrade = englishGrade;
+    }
+    
+    public double getScieneGrade(){
+        return scienceGrade;
+    }
+    
+    public void setScienceGrade(double englishGrade){
+        this.scienceGrade = scienceGrade;
+    }
+    
+    public double getAverage(){
+        return average;
+    }
+    
+    public static int getStudentCount(){
+        return studentCount;
+    }
+}
