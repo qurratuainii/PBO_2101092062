acbc45f16e190e23408b5087ee1c97d2f38128d1
qurratuainii <114158317+qurratuainii@users.noreply.github.com>
2022-11-04 11:09:33 +0700

    Create StudentRecordExample.java

---
+++ src/Aini041122/StudentRecordExample.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aini041122;
+
+/**
+ *
+ * @author HP Folio 1040
+ */
+public class StudentRecordExample {
+    
+}
