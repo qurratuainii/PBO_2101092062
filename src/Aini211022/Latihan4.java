9e7d1531b60d7b45f8aaa0d969fcb83cb181baf8
qurratuainii <114158317+qurratuainii@users.noreply.github.com>
2022-11-17 13:36:53 +0700

    Update Latihan4.java

--- src/Aini211022/Latihan4.java
+++ src/Aini211022/Latihan4.java
@@ -10,18 +10,28 @@ package Aini211022;
  * @author HP Folio 1040
  */
 
-import javax.swing.JOptionPane;
+import java.io.*;
 
 public class Latihan4 {
-    public static void main(String[] args) {
-        String a="";
-        String b="";
+    public static void main(String[] args) throws IOException {
+        BufferedReader In = new BufferedReader (
+                                new InputStreamReader(System.in));
         
-        a = JOptionPane.showInputDialog("Masukkan angka");
-     
-        b = JOptionPane.showInputDialog("Masukkan pangkat");
+        int h = 1;
         
+        System.out.print("Nilai angka : ");
+        int angka = Integer.parseInt(In.readLine());
         
+        System.out.print("Nilai pangkat : ");
+        int pangkat = Integer.parseInt(In.readLine());
         
+        int i =0;
+        
+        while (i<pangkat){
+            h = h * angka;
+            i++;
+        }
+        
+        System.out.println(angka + " ^ " + pangkat + " = "+h);
     }
 }
