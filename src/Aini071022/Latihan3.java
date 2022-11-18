a5ae4cd95dc29e093d5112795524ad1899deb4ce
qurratuainii <114158317+qurratuainii@users.noreply.github.com>
2022-11-18 11:48:21 +0700

    Update Latihan3.java

--- src/Aini071022/Latihan3.java
+++ src/Aini071022/Latihan3.java
@@ -10,6 +10,12 @@ package Aini071022;
  * @author HP Folio 1040
  */
 
+import java.io.*;
 
 public class Latihan3 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader In = new BufferedReader (
+                                new InputStreamReader(System.in));
+        
+    }
 }
