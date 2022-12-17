25ec55cea9c467efd677a8332a66054a86f15f1d
qurratuainii <114158317+qurratuainii@users.noreply.github.com>
2022-12-17 13:46:31 +0700

    Create MyInteger.java

---
+++ src/Aini161222/MyInteger.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aini161222;
+
+/**
+ *
+ * @author HP Folio 1040
+ */
+public class MyInteger implements Relation{
+    public boolean isGreater(Object a, Object b){
+        return (int)a > (int)b;
+    } 
+    public boolean isLess(Object a, Object b){
+        return (int)a < (int)b;
+    } 
+    public boolean isEqual(Object a, Object b){
+        return (int)a == (int)b;
+    }
+}
