86c1b8e3a902415ac244eecef7b4684184732836
qurratuainii <114158317+qurratuainii@users.noreply.github.com>
2023-01-06 21:51:16 +0700

    Update AnggotaController.java

--- src/Aini/controller/AnggotaController.java
+++ src/Aini/controller/AnggotaController.java
@@ -6,7 +6,9 @@
 package Aini.controller;
 import Aini.view.*;
 import Aini.model.*;
+import java.util.List;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -36,5 +38,44 @@ public class AnggotaController {
         anggota.setAlamat(formAnggota.getTxtAlamat().getText());
         anggotaDao.save(anggota);
         JOptionPane.showMessageDialog(formAnggota, "Insert OK");
+    }
+    
+    public void tampil(){
+        DefaultTableModel tabelModel = (DefaultTableModel) formAnggota.getTblAnggota().getModel();
+        tabelModel.setRowCount(0);
+        List<Anggota> list = anggotaDao.getAllAnggota();
+        for (Anggota anggota : list){
+            Object[] row = {
+                anggota.getNobp(),
+                anggota.getNama(),
+                anggota.getAlamat()
+            };
+            tabelModel.addRow(row);
+        }
+    }
+    
+    public void updateAnggota(){
+        int index = formAnggota.getTblAnggota().getSelectedRow();
+        anggota.setNobp(formAnggota.getTxtNobp().getText());
+        anggota.setNama(formAnggota.getTxtNama().getText());
+        anggota.setAlamat(formAnggota.getTxtAlamat().getText());
+        anggotaDao.update(index, anggota);
+        JOptionPane.showMessageDialog(formAnggota, "Update Ok");
+    }
+    
+    public void deleteAnggota(){
+        int index = formAnggota.getTblAnggota().getSelectedRow();
+        anggotaDao.delete(index);
+        JOptionPane.showMessageDialog(formAnggota, "Delete Ok");
+    }
+    
+    public void getAnggota(){
+        int index = formAnggota.getTblAnggota().getSelectedRow();
+        anggota = anggotaDao.getAnggota(index);
+        if(anggota != null){
+            formAnggota.getTxtNobp().setText(anggota.getNobp());
+            formAnggota.getTxtNama().setText(anggota.getNama());
+            formAnggota.getTxtAlamat().setText(anggota.getAlamat());
+        }
     }
 }
